dc9fd013cb78cf4f2fd31be8329e63bae4e47a47
Augustin Tacquet <augu.tacquet@gmail.com>
2022-08-12 10:55:05 +0200

    implemented landing page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,33 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/FishEye_Photos/Sample Photos/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+        const photographerLink = document.createElement('a')
+        photographerLink.setAttribute("href", "photographer.html?id=" + id)
+        photographerLink.setAttribute("aria-label", "Profil de " + name)
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
+        const h2 = document.createElement('h2');
+        const locationDiv = document.createElement('div');
+        const taglineDiv = document.createElement('div');
+        const priceDiv = document.createElement('div');
+        locationDiv.textContent = city + ", " + country;
+        taglineDiv.textContent = tagline;
+        priceDiv.textContent = price + "€/jour"
         h2.textContent = name;
-        article.appendChild(img);
+        article.appendChild(photographerLink);
+        photographerLink.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(locationDiv);
+        locationDiv.classList.add('location')
+        article.appendChild(taglineDiv);
+        article.appendChild(priceDiv);
+        priceDiv.classList.add('price')
+  
         return (article);
     }
     return { name, picture, getUserCardDOM }
