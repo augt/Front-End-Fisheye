a34e24328a5e750895007f759786a55499f87803
Augustin Tacquet <augu.tacquet@gmail.com>
2022-09-02 09:44:58 +0200

    started contact modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,16 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+    const main = document.getElementById("main");
+    main.setAttribute("aria-hidden", "true");
+    modal.setAttribute("aria-hidden", "false");
+
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+    const main = document.getElementById("main");
+    main.setAttribute("aria-hidden", "true");
+    modal.setAttribute("aria-hidden", "false");
 }
