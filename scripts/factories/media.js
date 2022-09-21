3b25ef5bb7805f5dc5e6634532042188dd6b3f8f
Augustin Tacquet <augu.tacquet@gmail.com>
2022-09-21 18:37:40 +0200

    code formating

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,42 +1,38 @@
 function mediaFactory(data, isLightbox) {
-    const { title } = data;
+  const { title } = data;
 
-    function getMediaCardDOM(photographerName) {
-        const mediaCardDOM = document.createElement('div');
-        if (isLightbox){
-            mediaCardDOM.classList.add("lightbox-card");
-        } else {
-            mediaCardDOM.classList.add("media-card");
-        }
-        
-
-        let mediaUrl = "";
-
-        if (data.hasOwnProperty("image")){
-            mediaUrl = `assets/FishEye_Photos/Sample Photos/${photographerName}/${data.image}`;
-            let image = document.createElement('img');
-            image.setAttribute("src", mediaUrl);
-            image.setAttribute("alt", title);
-            mediaCardDOM.appendChild(image);
-
-        } else {
-            mediaUrl = `assets/FishEye_Photos/Sample Photos/${photographerName}/${data.video}`;
-            let video = document.createElement('video');
-            video.setAttribute("aria-label", title)
-            if (isLightbox===true){
-                video.setAttribute("controls", true);
-            };
-            mediaCardDOM.appendChild(video);
-            let source = document.createElement('source');
-            source.setAttribute("src", mediaUrl);
-            source.setAttribute("type", "video/mp4");
-            video.appendChild(source);
-        }
-
-         
-  
-        return (mediaCardDOM);
+  function getMediaCardDOM(photographerName) {
+    const mediaCardDOM = document.createElement("div");
+    if (isLightbox) {
+      mediaCardDOM.classList.add("lightbox-card");
+    } else {
+      mediaCardDOM.classList.add("media-card");
     }
 
-    return { title, getMediaCardDOM }
+    let mediaUrl = "";
+
+    if (data.hasOwnProperty("image")) {
+      mediaUrl = `assets/FishEye_Photos/Sample Photos/${photographerName}/${data.image}`;
+      let image = document.createElement("img");
+      image.setAttribute("src", mediaUrl);
+      image.setAttribute("alt", title);
+      mediaCardDOM.appendChild(image);
+    } else {
+      mediaUrl = `assets/FishEye_Photos/Sample Photos/${photographerName}/${data.video}`;
+      let video = document.createElement("video");
+      video.setAttribute("aria-label", title);
+      if (isLightbox === true) {
+        video.setAttribute("controls", true);
+      }
+      mediaCardDOM.appendChild(video);
+      let source = document.createElement("source");
+      source.setAttribute("src", mediaUrl);
+      source.setAttribute("type", "video/mp4");
+      video.appendChild(source);
+    }
+
+    return mediaCardDOM;
+  }
+
+  return { title, getMediaCardDOM };
 }
--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,34 +1,34 @@
 function photographerFactory(data) {
-    const { name, portrait, id, city, country, tagline, price } = data;
+  const { name, portrait, id, city, country, tagline, price } = data;
 
-    const picture = `assets/FishEye_Photos/Sample Photos/Photographers ID Photos/${portrait}`;
+  const picture = `assets/FishEye_Photos/Sample Photos/Photographers ID Photos/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement('article');
-        const photographerLink = document.createElement('a')
-        photographerLink.setAttribute("href", "photographer.html?id=" + id)
-        photographerLink.setAttribute("aria-label", "Profil de " + name)
-        const img = document.createElement('img');
-        img.setAttribute("src", picture);
-        img.setAttribute("alt", name);
-        const h2 = document.createElement('h2');
-        const locationDiv = document.createElement('div');
-        const taglineDiv = document.createElement('div');
-        const priceDiv = document.createElement('div');
-        locationDiv.textContent = city + ", " + country;
-        taglineDiv.textContent = tagline;
-        priceDiv.textContent = price + "€/jour"
-        h2.textContent = name;
-        article.appendChild(photographerLink);
-        photographerLink.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(locationDiv);
-        locationDiv.classList.add('location')
-        article.appendChild(taglineDiv);
-        article.appendChild(priceDiv);
-        priceDiv.classList.add('price')
-  
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const photographerLink = document.createElement("a");
+    photographerLink.setAttribute("href", "photographer.html?id=" + id);
+    photographerLink.setAttribute("aria-label", "Profil de " + name);
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", name);
+    const h2 = document.createElement("h2");
+    const locationDiv = document.createElement("div");
+    const taglineDiv = document.createElement("div");
+    const priceDiv = document.createElement("div");
+    locationDiv.textContent = city + ", " + country;
+    taglineDiv.textContent = tagline;
+    priceDiv.textContent = price + "€/jour";
+    h2.textContent = name;
+    article.appendChild(photographerLink);
+    photographerLink.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(locationDiv);
+    locationDiv.classList.add("location");
+    article.appendChild(taglineDiv);
+    article.appendChild(priceDiv);
+    priceDiv.classList.add("price");
+
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
